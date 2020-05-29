2da9534710f60ad475236fdeb9fcc55d4ddb2603
C9-LinkRs <linkrsfan@gmail.com>
2020-05-29 23:36:17 +0200

    Bot - Welcome embed message done

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const serverListener = require("./server");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -5,8 +6,29 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", message => {
-  if (message.content === "Ping!") message.reply("Pong!");
+client.on("guildMemberAdd", member => {
+  console.log(member)
+  let guild = member.guild;
+  let user = member.user;
+  if (guild.systemChannel) {
+    guild.systemChannel.send(new Discord.MessageEmbed()
+    .setTitle("A new user joined!")
+    .setDescription(`${user.username} has joined the guild`)
+    .setThumbnail(member.user.displayAvatarURL)
+    .addField("Members now", guild.memberCount)
+    .setTimestamp()
+    );
+  }
 });
+
+client.on("message", message => {
+  if (message.content === "!ping") message.reply("Pong!");
+});
+
+async function onNotification(message) {
+
+}
+
+serverListener.on("turn", onNotification);
 
 client.login(process.env.BOT_TOKEN);
